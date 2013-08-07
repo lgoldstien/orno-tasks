e2091a4100216555a59315ee905c0eeff52b9fb5
Lawrence Goldstien <lawrence.goldstien@symphony-group.co.uk>
2013-08-07 17:14:19 +0100

    Start building in backbone base user interface

---
+++ modules/App/views/home/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="panel">
+        <div class="panel-heading">
+            <h3 class="panel-title">Task Title</h3>
+        </div>
+        <div class="panel-conent">
+            <p>Some random task that you will never actually get done!</p>
+        </div>
+    </div>
+</div>
--- modules/App/views/layouts/default.php
+++ modules/App/views/layouts/default.php
@@ -4,13 +4,27 @@
         <meta charset="utf-8">
         <title>Tasks</title>
         <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/vendor/bootstrap.min.css">
     </head>
     <body>
         <header>
-            <h1>Tasks</h1>
+            <div class="navbar">
+                <a class="navbar-brand" href="/">Tasks</a>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#viewCurrent">Current</a></li>
+                    <li><a href="#viewToday">Due Today</a></li>
+                    <li><a href="#viewComplete">Complete</a></li>
+                </ul>
+            </div>
         </header>
         <section>
             <?= $this->region('content') ?>
         </section>
+
+        <script src="script/vendor/jquery/jquery.min.js"></script>
+        <script src="script/vendor/underscore/underscore.min.js"></script>
+        <script src="script/vendor/backbone/backbone.min.js"></script>
+        <script src="script/vendor/bootstrap/bootstrap.min.js"></script>
+        <script src="script/app.js"></script>
     </body>
 </html>
